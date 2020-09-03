81b43115601a8b8926b8bbc2094d9cac7558e873
Letícia Medeiros <67078601+leticiaam@users.noreply.github.com>
2020-09-03 19:24:39 -0300

    aula05

--- src/fiado/main.java
+++ src/fiado/main.java
@@ -4,31 +4,29 @@ import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
-		int[] fiados = new int [10];
-//		fiados[0] = 12;
-//		fiados[3] = 87;
-		
+public static void main(String[] args) {
+	
+	Consumidor consumidor = new Consumidor();
+	consumidor.fiados = new int[10];
+	
+
 
 	
 	Scanner scanner = new Scanner(System.in);
 		 System.out.print  ("informe o nome do cliente: ");
-		 String nome = scanner.nextLine();
+		 consumidor.nome = scanner.nextLine();
 		 System.out.print("Informe o valor da compra: ");
-		 fiados[0] = Integer.valueOf(scanner.nextLine());
+		 consumidor.fiados[0] = Integer.valueOf(scanner.nextLine());
 		 
-		 int total = somaWhile(fiados);
+		 int total = somaWhile(consumidor.fiados);
 		 
 		 if (total > 100) {
-			 System.out.println("Cliente " + nome + " deve: " + total + " GANHOU BRINDE");
+			 System.out.println("Cliente " + consumidor.nome + " deve: " + total + " GANHOU BRINDE");
 			 } else {
-			 System.out.println("Cliente " + nome + " deve: " + total + " NÃO GANHOU BRINDE");
+			 System.out.println("Cliente " + consumidor.nome + " deve: " + total + " NÃO GANHOU BRINDE");
 			 }
 		 }
 		 
-		 
-	
-	
 	public static int somaWhile(int[] fiados) {
 		int i = 0;
 		int total = 0;
@@ -38,19 +36,19 @@ public class main {
 		}
 		return total;
 		}
-	public static int somaFor(int[] fiados) {
-		int total = 0;
-		for (int i=0; i < fiados.length; i++) {
-			total += fiados [i];
-		}
-	return total;
-	}
-	public static int somaForeach(int[] fiados) {
-		int total = 0;
-		for (int fiado : fiados) {
-			total += fiado;
-		}
-		return total;
-		}
-		
+//	public static int somaFor(int[] fiados) {
+//		int total = 0;
+//		for (int i=0; i < fiados.length; i++) {
+//			total += fiados [i];
+//		}
+//	return total;
+//	}
+//	public static int somaForeach(int[] fiados) {
+//		int total = 0;
+//		for (int fiado : fiados) {
+//			total += fiado;
+//		}
+//		return total;
+//}	
 }
+
