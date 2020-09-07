e3fc2d138e67996ebe4cc7160a2d3afaa7a67556
Letícia Medeiros <67078601+leticiaam@users.noreply.github.com>
2020-09-07 14:17:38 -0300

    Adiciona telefone ao consumidor

--- src/fiado/Consumidor.java
+++ src/fiado/Consumidor.java
@@ -2,7 +2,43 @@ package fiado;
 
 public class Consumidor {
 	
-	String nome;
-	int[] fiados;
-
+	private String nome;
+	private int[] fiados;
+	private int posicao = 0;
+	private String telefone;
+	
+	public Consumidor(String nome, String telefone) {
+		this.nome = nome;
+		this.fiados = new int[10];
+		this.telefone = telefone;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getTelefone () {
+		return this.telefone;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public void registrarFiado(int valor) {
+		this.fiados[posicao] = valor;
+		posicao++;
+	}
+	
+	public int getFiado () {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;
+		}
+		return total;
+	}
 }
--- src/fiado/main.java
+++ src/fiado/main.java
@@ -4,39 +4,41 @@ import java.util.Scanner;
 
 public class main {
 
-public static void main(String[] args) {
-	
-	Consumidor consumidor = new Consumidor();
-	consumidor.fiados = new int[10];
-	
+	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("informe o nome do cliente: ");
+		String nome = scanner.nextLine();
 
-	
-	Scanner scanner = new Scanner(System.in);
-		 System.out.print  ("informe o nome do cliente: ");
-		 consumidor.nome = scanner.nextLine();
-		 System.out.print("Informe o valor da compra: ");
-		 consumidor.fiados[0] = Integer.valueOf(scanner.nextLine());
-		 
-		 int total = somaWhile(consumidor.fiados);
-		 
-		 if (total > 100) {
-			 System.out.println("Cliente " + consumidor.nome + " deve: " + total + " GANHOU BRINDE");
-			 } else {
-			 System.out.println("Cliente " + consumidor.nome + " deve: " + total + " NÃO GANHOU BRINDE");
-			 }
-		 }
-		 
+		System.out.print("informe o telefone do cliente: ");
+		String telefone = scanner.nextLine();
+
+		Consumidor consumidor = new Consumidor(nome, telefone);
+
+		System.out.print("Informe o valor da compra: ");
+		int fiado = Integer.valueOf(scanner.nextLine());
+		consumidor.registrarFiado(fiado);
+
+		int total = consumidor.getFiado();
+
+		if (total > 100) {
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " GANHOU BRINDE");
+		} else {
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " NÃO GANHOU BRINDE");
+		}
+	}
+
 	public static int somaWhile(int[] fiados) {
 		int i = 0;
 		int total = 0;
 		while (i < fiados.length) {
-			total += fiados [i];
+			total += fiados[i];
 			i++;
 		}
 		return total;
-		}
-//	public static int somaFor(int[] fiados) {
+	}
+
+//		public static int somaFor(int[] fiados) {
 //		int total = 0;
 //		for (int i=0; i < fiados.length; i++) {
 //			total += fiados [i];
@@ -51,4 +53,3 @@ public static void main(String[] args) {
 //		return total;
 //}	
 }
-
